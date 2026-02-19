9b9788de1b313252f5a7977e41a5c47c64774bcc
Fabrice Morin <fabrice.morin@live.com>
2026-02-19 17:50:59 +0100

    Use "official" github repo for hungarian algorithm

--- common/HungarianAlgorithm.cmake
+++ common/HungarianAlgorithm.cmake
@@ -1,5 +1,15 @@
-include(FetchContent)
-FetchContent_Declare(HungarianAlgorithm
-  GIT_REPOSITORY git@github.com:jinac/hungarian-algorithm-cpp.git
+include(ExternalProject)
+find_program(MAKE_EXE NAMES gmake nmake make)
+
+set(TARGET_HUNGARIAN HungarianAlgorithm)
+
+ExternalProject_Add(${TARGET_HUNGARIAN}
+  GIT_REPOSITORY    git@github.com:mcximing/hungarian-algorithm-cpp.git
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  SOURCE_DIR ${CMAKE_BINARY_DIR}/${TARGET_HUNGARIAN}
+#  BUILD_IN_SOURCE 1
 )
-FetchContent_MakeAvailable(HungarianAlgorithm)
+
+add_library(hungarian ${CMAKE_BINARY_DIR}/${TARGET_HUNGARIAN}/Hungarian.cpp ${CMAKE_BINARY_DIR}/${TARGET_HUNGARIAN}/Hungarian.h)
