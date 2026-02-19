fc4038767f528fdd14147fca8b0750e565736321
Fabrice Morin <fabrice.morin@live.com>
2026-02-19 09:56:57 +0100

    Add cmake files for dependencies

---
+++ app/QHotkey.cmake
@@ -0,0 +1,6 @@
+include(FetchContent)
+FetchContent_Declare(
+  QHotkey
+  GIT_REPOSITORY git@github.com:Skycoder42/QHotkey.git
+)
+FetchContent_MakeAvailable(QHotkey)
---
+++ common/HungarianAlgorithm.cmake
@@ -0,0 +1,5 @@
+include(FetchContent)
+FetchContent_Declare(HungarianAlgorithm
+  GIT_REPOSITORY git@github.com:jinac/hungarian-algorithm-cpp.git
+)
+FetchContent_MakeAvailable(HungarianAlgorithm)
